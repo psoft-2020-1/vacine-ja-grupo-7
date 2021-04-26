dbd6b4517a812ff00a526f4e443fad3e0dd64153
Pedro Silva <pedro.henrique.silva@ccc.ufcg.edu.br>
2021-04-26 19:49:23 -0300

    Commit inicial, com configuração do projeto.

---
+++ src/main/java/com/ufcg/psoft/vacinaja/VacinaJaApplication.java
@@ -0,0 +1,13 @@
+package com.ufcg.psoft.vacinaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinaJaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinaJaApplication.class, args);
+	}
+
+}
