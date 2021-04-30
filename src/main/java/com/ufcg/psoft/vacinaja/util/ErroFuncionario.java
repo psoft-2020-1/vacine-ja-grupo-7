41d62038ce4330b3c91b3bed72dcbe2d88b0c2b2
carloshgr <carlos.ribeiro@ccc.ufcg.edu.br>
2021-04-30 19:02:32 -0300

    adiciona mais erros customizados para validacao do cadastro de funcionario

--- src/main/java/com/ufcg/psoft/vacinaja/util/ErroFuncionario.java
+++ src/main/java/com/ufcg/psoft/vacinaja/util/ErroFuncionario.java
@@ -15,9 +15,57 @@ public class ErroFuncionario {
 	
 	static final String ERRO_LOCAL_DE_TRABALHO_NULO = "O local de trabalho não pode ser nulo!";
 	
+	static final String ERRO_CPF_VAZIO = "O CPF não pode ser vazio!";
+	
+	static final String ERRO_CARGO_VAZIO = "O cargo não pode ser vazio!";
+	
+	static final String ERRO_LOCAL_DE_TRABALHO_VAZIO = "O local de trabalho não pode ser vazio!";
+	
+	public static ResponseEntity<CustomErrorType> erroCidadaoNaoCadastrado() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_CIDADAO_NAO_CADASTRADO), 
+				HttpStatus.NOT_FOUND);
+	}
+	
 	public static ResponseEntity<CustomErrorType> erroFuncionarioJaCadastrado() {
 		return new ResponseEntity<CustomErrorType>(
 				new CustomErrorType(ERRO_FUNCIONARIO_JA_CADASTRADO), 
 				HttpStatus.CONFLICT);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroCpfFuncionarioNulo() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_CPF_NULO), 
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroCargoFuncionarioNulo() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_CARGO_NULO), 
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroLocalDeTrabalhoFuncionarioNulo() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_LOCAL_DE_TRABALHO_NULO), 
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroCpfFuncionarioVazio() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_CPF_VAZIO), 
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroCargoFuncionarioVazio() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_CARGO_VAZIO), 
+				HttpStatus.BAD_REQUEST);
+	}
+	
+	public static ResponseEntity<CustomErrorType> erroLocalDeTrabalhoFuncionarioVazio() {
+		return new ResponseEntity<CustomErrorType>(
+				new CustomErrorType(ERRO_LOCAL_DE_TRABALHO_VAZIO), 
+				HttpStatus.BAD_REQUEST);
 	}
 }
