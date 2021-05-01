187d8da0d03bab103a31ba557016155dc0bfef09
Lucian <lucian.costa.ext@synchro.com.br>
2021-05-01 16:03:07 -0300

    finalizando a US3

---
+++ src/main/java/com/ufcg/psoft/vacinaja/service/CidadaoService.java
@@ -0,0 +1,14 @@
+package com.ufcg.psoft.vacinaja.service;
+
+import com.ufcg.psoft.vacinaja.dto.CidadaoDTO;
+import com.ufcg.psoft.vacinaja.model.Cidadao;
+
+import java.util.Optional;
+
+public interface CidadaoService {
+
+    public Optional<Cidadao> buscarCidadaoPeloCpf(String cpf);
+
+    public Cidadao cadastrarCidadao(CidadaoDTO cidadao);
+
+}
