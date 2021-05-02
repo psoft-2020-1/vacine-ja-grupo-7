54cd72c8e17117be5f9084eb71b589c5f9e12d08
carloshgr <carlos.ribeiro@ccc.ufcg.edu.br>
2021-05-02 19:49:19 -0300

    finaliza us4, adiciona testes de unidade do cadastro de cidadaos e funcionarios

---
+++ src/main/java/com/ufcg/psoft/vacinaja/exceptions/UsuarioInvalidoException.java
@@ -0,0 +1,7 @@
+package com.ufcg.psoft.vacinaja.exceptions;
+
+public class UsuarioInvalidoException extends RuntimeException {
+	public UsuarioInvalidoException(String message) {
+        super(message);
+	}
+}
