c7dde056654194f2caac49b18031de9250390b0e
Eniedson Junior <eniedson.silva@gmail.com>
2021-05-02 17:21:42 -0300

    Login commit inicial

---
+++ src/main/java/com/ufcg/psoft/vacinaja/controller/UsuarioController.java
@@ -0,0 +1,20 @@
+package com.ufcg.psoft.vacinaja.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class UsuarioController {
+
+	@RequestMapping("/users")
+	@ResponseBody
+	public String getUsers() {
+		return "{\"users\":[{\"name\":\"Lucas\", \"country\":\"Brazil\"}," +
+		           "{\"name\":\"Jackie\",\"country\":\"China\"}]}";
+	}
+	
+}
