015e12c065a37b353bc53067349f9ab65e902bf1
Pedro Silva <pedro.henrique.silva@ccc.ufcg.edu.br>
2021-05-14 18:51:49 -0300

    Descomentando código.

--- src/main/java/com/ufcg/psoft/vacinaja/VacinaJaApplication.java
+++ src/main/java/com/ufcg/psoft/vacinaja/VacinaJaApplication.java
@@ -10,14 +10,14 @@ import com.ufcg.psoft.vacinaja.security.TokenFilter;
 @SpringBootApplication
 public class VacinaJaApplication {
 
-//	@Bean
-//	public FilterRegistrationBean<TokenFilter> filterJwt() {
-//		FilterRegistrationBean<TokenFilter> filterRB = new FilterRegistrationBean<TokenFilter>();
-//		filterRB.setFilter(new TokenFilter());
-//		filterRB.addUrlPatterns("/api/usuario/*", "/api/administrador/*", "/api/cidadao/*", "/api/comorbidade/*",
-//				"/api/funcionario/*", "/api/lote/*", "/api/registro-vacinacao/*", "/api/vacina/*", "/api/vacinas/");
-//		return filterRB;
-//	}
+	@Bean
+	public FilterRegistrationBean<TokenFilter> filterJwt() {
+		FilterRegistrationBean<TokenFilter> filterRB = new FilterRegistrationBean<TokenFilter>();
+		filterRB.setFilter(new TokenFilter());
+		filterRB.addUrlPatterns("/api/usuario/*", "/api/administrador/*", "/api/cidadao/*", "/api/comorbidade/*",
+				"/api/funcionario/*", "/api/lote/*", "/api/registro-vacinacao/*", "/api/vacina/*", "/api/vacinas/");
+		return filterRB;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VacinaJaApplication.class, args);
