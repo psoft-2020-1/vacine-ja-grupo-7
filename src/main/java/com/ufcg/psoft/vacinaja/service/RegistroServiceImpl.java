1bd2c3ddd400c40091782f96d03ddc3aa977f7f7
carloshgr <carlos.ribeiro@ccc.ufcg.edu.br>
2021-05-15 17:10:09 -0300

    continua implementacao US16

--- src/main/java/com/ufcg/psoft/vacinaja/service/RegistroServiceImpl.java
+++ src/main/java/com/ufcg/psoft/vacinaja/service/RegistroServiceImpl.java
@@ -4,13 +4,17 @@ import com.ufcg.psoft.vacinaja.exceptions.CidadaoInvalidoException;
 import com.ufcg.psoft.vacinaja.exceptions.RegistroInvalidoException;
 import com.ufcg.psoft.vacinaja.exceptions.VacinaInvalidaException;
 import com.ufcg.psoft.vacinaja.model.Cidadao;
+import com.ufcg.psoft.vacinaja.model.Lote;
 import com.ufcg.psoft.vacinaja.model.RegistroVacinacao;
 import com.ufcg.psoft.vacinaja.model.Usuario;
 import com.ufcg.psoft.vacinaja.model.Vacina;
 import com.ufcg.psoft.vacinaja.repository.CidadaoRepository;
+import com.ufcg.psoft.vacinaja.repository.LoteRepository;
 import com.ufcg.psoft.vacinaja.repository.RegistroRepository;
 import com.ufcg.psoft.vacinaja.repository.UsuarioRepository;
 import com.ufcg.psoft.vacinaja.repository.VacinaRepository;
+import com.ufcg.psoft.vacinaja.states.VacinacaoState;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -30,6 +34,9 @@ public class RegistroServiceImpl implements RegistroService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
+    
+    @Autowired
+    private LoteRepository loteRepository;
 
     @Override
     public RegistroVacinacao vacinar(String cpfCidadao, Long vacinaId) {
@@ -46,8 +53,16 @@ public class RegistroServiceImpl implements RegistroService {
         Optional<Usuario> usuario = usuarioRepository.getUsuarioByCadastroCidadao(cidadaoOptional.get());
 
         String email = usuario.get().getEmail();
-
-        RegistroVacinacao registroRetorno = registroVacinacao.vacinar(vacinaOptional.get(), email);
+        
+        Vacina vacina = vacinaOptional.get();
+        VacinacaoState estadoVacinacao = registroVacinacao.getEstadoVacinacao();
+        
+        Optional<Lote> optionalLote = loteRepository.proximoLoteByState(estadoVacinacao, vacina);
+        Lote lote = optionalLote.get();
+        
+        //SE O LOTE VIER VAZIO É PQ NÃO TEM DOSES OU O STATE NÃO É O CERTO PARA VACINAR
+        
+        RegistroVacinacao registroRetorno = registroVacinacao.vacinar(vacinaOptional.get(), email, lote);
         return registroRepository.save(registroRetorno);
     }
 }
